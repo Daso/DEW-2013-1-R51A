4dfa8e85ecd2cca12467d982ea54a00bf20c1d68
c <pcsidsub@E54-SERV.upc.edu.pe>
2013-05-06 21:17:30 -0500

    clase 02

---
+++ clase02/moms_day/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MomsDay::Application.config.secret_token = 'df681abb7af1618bbf42ebcd7152e28fa2b8114ca0cfaeb269d0d20eada9ca886d1457e8123eaf8a1a9d8b15de23de98a1cb99e522d790b46787c9837feec496'
---
+++ clase02/moms_day/db/migrate/20130507010544_add_mother_id_to_gift.rb
@@ -0,0 +1,6 @@
+class AddMotherIdToGift < ActiveRecord::Migration
+  def change
+    add_column :gifts, :mother_id, :integer
+
+  end
+end
